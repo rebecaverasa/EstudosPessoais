e743e57e9f74be8e65c0b46623ab93259822e681
rebecaverasa <rebecaverasa@gmail.com>
2026-09-16 00:33:09 -0300

    2665. Counter II

---
+++ leetcode/2665.CounterII.js
@@ -0,0 +1,29 @@
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function(init) {
+    let value = init;
+    let initialValue = init;
+    return {
+        increment(){
+            value = value + 1;
+            return value;
+        },
+        decrement(){
+            value = value - 1;
+            return value;
+        },
+        reset(){
+            value = initialValue;
+            return value;
+        }
+    }
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
